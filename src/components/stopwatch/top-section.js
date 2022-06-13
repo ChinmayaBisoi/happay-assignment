84b634ea0830076f1f29fee12ab19d818562776e
ChinmayaBisoi <bisoi.chinmaya.1999@gmail.com>
2022-06-13 22:49:12 +0530

    app structure changes

---
+++ src/components/stopwatch/top-section.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SwTopSection = () => {
+  return (
+    <div className="py-16 bg-[#06283D] text-[#47B5FF] px-auto">
+      SwTopSection
+    </div>
+  );
+};
+
+export default SwTopSection;
